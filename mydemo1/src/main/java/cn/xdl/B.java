82d89f2755f03152ce9ffe1b0d40b71804d58253
liangjianquan <ljq@itxdl.cn>
2019-11-14 12:12:02 +0800

    add and modify

--- mydemo1/src/main/java/cn/xdl/B.java
+++ mydemo1/src/main/java/cn/xdl/B.java
@@ -4,6 +4,7 @@ public class B {
 
 	public static void main(String[] args) {
 		System.out.println("BBBBBBBBBBBb");
+		System.out.println("BBBBBBBBBBBb");
 	}
 
 }
---
+++ mydemo1/src/main/java/cn/xdl/C.java
@@ -0,0 +1,5 @@
+package cn.xdl;
+
+public class C {
+
+}
